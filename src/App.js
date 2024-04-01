6a3c17f73048e01afd2be5f3a3a05104649b8caa
Ndathi08 <hellen.ndathi@strathmore.edu>
2024-04-01 17:02:39 +0300

    Passing data deeply with LevelContext

--- src/App.js
+++ src/App.js
@@ -4,17 +4,31 @@ import AboutPage from './Components/AboutPage';
 import Profile from './Components/Profile';
 import ShoppingList from './Components/ShoppingList';
 import MyApp from './Components/MyApp';
+import Heading from './Components/Heading';
+import Section from './Components/Section';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to my app</h1>
-      <MyButton/>
-      <AboutPage/>
-      <Profile/>
-      <ShoppingList/>
-      <MyButton/>
-      <MyApp/>
+      <Section>
+      <Heading>Title</Heading>
+      <Section>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Section>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Section>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+          </Section>
+        </Section>
+      </Section>
+    </Section>
     </div>
   );
 }
