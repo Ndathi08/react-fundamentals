f223afff49cfd6ba65f66c7320d3f8f905ae3036
Ndathi08 <hellen.ndathi@strathmore.edu>
2024-04-01 09:42:02 +0300

    Adding style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import MyButton from './Components/MyButton';
+import AboutPage from './Components/AboutPage';
+import Profile from './Components/Profile';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to my app</h1>
       <MyButton/>
+      <AboutPage/>
+      <Profile/>
     </div>
   );
 }
